e3567e34a5176737f9b30ada0ca85a78da73704c
Jihad Dayoub <pythoner1991@gmail.com>
2023-02-01 20:14:13 +0300

    Added menu functionality

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu', views.MenuListView.as_view(), name='menu-list'),
+    path('menu/<int:pk>', views.MenuDetailView.as_view(), name='menu-detail'),
 ]
+
+urlpatterns += router.urls
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import (
+    UserSerializer,
+    MenuSerializer,
+    BookingSerializer
+)
+from .models import (
+    Menu,
+    Booking
+)
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuListView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class MenuDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
