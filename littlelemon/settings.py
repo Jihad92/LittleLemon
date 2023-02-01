093be1ba67e10fd2d9c5b0954778a8bb7f6882df
Jihad Dayoub <pythoner1991@gmail.com>
2023-02-01 20:35:57 +0300

    User authentication added

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
 
@@ -134,3 +136,14 @@ STATIC_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES ': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username'
+}
